6d0fb7ba2d8c232ccf1c0c9d9691ac3df2312f45
Eunhyung Choi <comdown_1003@naver.com>
2015-11-05 02:42:27 +0900

    기본 2-1모델 테스트후 3-1로 변경해서 테스트해보고 TODO할것들 추가함.

--- NumberClassificationTest/NumberClassificationTest/ViewController.m
+++ NumberClassificationTest/NumberClassificationTest/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "MLPNeuralNet.h"
+
 @interface ViewController ()
 
 @end
@@ -17,6 +19,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Use the designated initialiser to pass the network configuration and weights to the model.
+    // Note: You do not need to specify the biased units (+1 above) in the configuration.
+    
+    NSArray *netConfig = @[@3, @1];
+    double wts[] = {-30, 20, 20, 20};
+    NSData *weights = [NSData dataWithBytes:wts length:sizeof(wts)];
+    
+    MLPNeuralNet *model = [[MLPNeuralNet alloc] initWithLayerConfig:netConfig
+                                                            weights:weights
+                                                         outputMode:MLPClassification];
+    
+    // Predict output of the model for new sample
+    double sample[] = {1, 1, 0};
+    NSData * vector = [NSData dataWithBytes:sample length:sizeof(sample)];
+    NSMutableData * prediction = [NSMutableData dataWithLength:sizeof(double)];
+    
+    [model predictByFeatureVector:vector intoPredictionVector:prediction];
+    
+    double * assessment = (double *)prediction.bytes;
+    NSLog(@"Model assessment is %f", assessment[0]);
+    
+    NSLog(@"D-%@",[model description]);
+    
+    // 진행사항 : 3-1 network 실행테스트만 해봄
+    // TODO 1
+    // 앞으로 할것 이상태에서 http://destiny738.tistory.com/m/post/455 참고하여
+    // 2-1 network 에서 가중치 학습으로 로직추가하기
+    // TODO 2
+    // 1이 완료된다면 sample 경우의수를 전부다 컨테이너에 넣고 루프를 수행할때마다 선택해서 수행하게 변경하기
+    // TODO 3
+    // 여기에서는 아 몰랑 자야겠다.
 }
 
 - (void)didReceiveMemoryWarning {
